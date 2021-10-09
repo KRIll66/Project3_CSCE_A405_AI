280675c4010a4b1270f1a4c3d1c624063b0897c5
Marshall Pratt <marshallpratt1@yahoo.com>
2021-10-09 10:47:51 -0800

    adding a working owari board, all scoring works. Adding main with user input

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
 import owariBoard
 
 def main ():
-    game = owariBoard.owariBoard()
-
+    game = owariBoard.owariBoard()    
+    
+    #get the beginning input for the game to begin, allow players to select who goes first    
+    print ("Welcome to the Owari game, below is the starting board:")
     game.display()
+    first = input("Who would you like to go first, North or South?\nEnter 'n' for North or 's' for South: ")
+    while first != "n" and first != "s":        
+        print ("Sorry, you must use 'n' for north or 's' for South")
+        first = input("Who would you like to go first, North or South?\nEnter 'n' for North or 's' for South: ")
+        print ("You entered: ", first)
+    turn = first
+
+    #This loop handles game play, continues until a winner is decided
+    #All input is handled by user input
+    while True:
+        #first, check if it's norths turn
+        if turn == "n":
+            while True:
+                try:
+                    #get a valid move for North, must not be an empty reference and must be 7-12 inclusive
+                    move_index = int(input("It is North's turn, please select a move from 7-12 (remember that north side counts up from right to left:"))
+                    if move_index < 7 or move_index > 12:
+                        print ("This is North's turn, input must be 7-12 inclusive")
+                    elif game.board[move_index] == 0:
+                        print ("This is not a valid move, the dish you selected is empty...")
+                    else: break
+                except ValueError:
+                    print ("The input must be an integer...")
+            turn = "s"
+
+        #otherwise, it's south's turn
+        else:
+            while True:
+                try:
+                    #get a valid move for South, must not be an empty reference and must be 0-5 inclusive
+                    move_index = int(input("It is South's turn, please select a move from 0-5 (remember that south side counts up from left to right:"))
+                    if move_index < 0 or move_index > 5:
+                        print ("This is South's turn, input must be 0-5 inclusive")
+                    elif game.board[move_index] == 0:
+                        print ("This is not a valid move, the dish you selected is empty...")
+                    else: break
+                except ValueError:
+                    print ("The input must be an integer...")
+            turn = "n"
+
+        #make the move on the board and display the new game state
+        game.move(move_index)
+        game.display()
+        #check to see if gameover conditions have been met
+        if game.gameOver(): break
 
 
 if __name__ == '__main__':
--- owariBoard.py
+++ owariBoard.py
@@ -24,53 +24,142 @@ class owariBoard:
     
     def __init__(self):
         #board represents the current state of the game
-        self.board = [3,3,3,3,3,3,0,3,3,3,3,3,3,0]        
-        #this is the right of the board
-        self.north_goal = 0
-        #this is the left of the board
-        self.south_goal = 0
+        self.board = [3,3,3,3,3,3,0,3,3,3,3,3,3,0]                
         self.alpha = 0
         self.beta = 0
-        self.game_over = False
+        #capture maps link the opposite dishes to one another
+        #these are delcared in the initiator so we don't waste time
+        #and space declaring them with every possible move
+        self.capture_map_south = {
+            0: 12,
+            1: 11,
+            2: 10,
+            3: 9,
+            4: 8,
+            5: 7
+        }
+        self.capture_map_north = {
+            7: 5,
+            8: 4,
+            9: 3,
+            10: 2,
+            11: 1,
+            12: 0
+        }
+        
+
+    def getFinalScore(self):
+        north_list = self.board[7:13]
+        south_list = self.board[0:6]
+        south_goal = self.board[6]
+        north_goal = self.board[13]
+        #to tabulate the final score, whoever has stones left their side gets to conut them as points
+        for v in north_list:
+            north_goal += v
+        for v in south_list:
+            south_goal += v
+
+        if south_goal == north_goal: 
+            print("It's a tie!!")
+            print ("Each side has a score of: ", south_goal)
+        elif south_goal > north_goal:
+            print ("Hooray!! South wins!!")
+            print ("South's score: ", south_goal)
+            print ("North's score: ", north_goal)
+        else:
+            print ("North has bested South for the last time!")
+            print ("South's score: ", south_goal)
+            print ("North's score: ", north_goal)
+        
+
+    #This checks for conditions that end a game, if met then it calls the getFinalScore method
+    def gameOver(self):              
+        north_list = self.board[7:13]
+        south_list = self.board[0:6]        
+        #check if north is empty, if so then game is over
+        if all (v == 0 for v in north_list): 
+            self.getFinalScore()
+            return True
+
+        #next check if south is empty
+        if all (v == 0 for v in south_list):
+            self.getFinalScore()
+            return True
 
 
+    def capture (self, turn, index):
+        
+        if turn == "south":
+            capture_index = self.capture_map_south.get(index)
+            if self.board[capture_index] != 0:
+                print ("South gets to capture!!")
+                self.board[6] += self.board[capture_index]
+                self.board[capture_index] = 0
 
+        if turn == "north":
+            capture_index = self.capture_map_north.get(index)
+            if self.board[capture_index] != 0:
+                print ("North gets to capture!!")
+                self.board[13] += self.board[capture_index]
+                self.board[capture_index] = 0
+
+    
+    
     def move (self, index):
         
         #get the value at the selected index, this is the number of available moves
-        num_moves = self.board[index]
+        num_moves = self.board[index]        
         self.board[index]=0
+        
         if index < 6:
             turn = "south"
-        else: turn = "north"
+        else: turn = "north"        
         
         while num_moves > 0:
+           
             #this means that moves increment the index, north side moves decrement the index
-            if index < 7:                         
+            if index < 7: 
+                index +=1                        
                 #we are south and this is the opponents goal, don't increment it, move back to index of 0
-                if index == 6 and turn == "north":
-                    index = 13           
+                #if index == 6 and turn == "north":
+                #    index = 13           
                 #set index to 13 as that is the far right of the north side moves, log a goal for team south
                 if index == 6 and turn == "south":
-                    self.board[index]+=1
-                    index = 13
+                    self.board[index]+=1                   
                     #we are still in the south side, increment the value of the dish at index
-                else: self.board[index]+=1
+                else: 
+                    self.board[index]+=1                    
 
-            #this means that we are in the north side of the board, moves now decrement the index
+            #this means that we are in the north side of the board
             if index > 6:
+                index+=1
                 #this is the opponent and they have just scored, log score and move to index 0
                 if index == 13 and turn == "north":
                     self.board[index]+=1
-                    index = 0                
-                #set index to 0 as that is the far left of the south side moves
-                if index == 13 and turn == "south":
-                    index = 0
-        #moves complete, check to see if the final move landed in an empty dish
+                    index = -1
+                
+                else: 
+                    if index == 13: index = 0
+                    self.board[index]+=1                     
+                    
+            #performed a move, decrement move count
+            num_moves-=1
+
+        #moves complete, check to see if the final move landed in an empty dish on the player's side
+        #if so, then all the stone's opposite now belong to the player
+        if self.board[index] == 1:
+            if turn == "south" and index <6:
+                self.capture(turn, index)
+            if turn == "north" and index > 6 and index < 13:
+                self.capture(turn, index)
+
 
 
     def display (self):
         #display the board in a fashion that is clear and readable
-        print (" ", self.board[7:13])
+        #first, we must reverse the north side of the list since it's indexes move from right to left
+        north_list = self.board[7:13]
+        north_list.reverse()
+        print (" ", north_list)
         print (self.board[13], "                  ", self.board[6])
         print (" ", self.board[0:6])
